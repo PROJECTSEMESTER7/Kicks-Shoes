3dcf76ec27f1fbd4cb9276481c812c45a93d88a4
Đặng Quang Cường <155418798+CuongDangCode@users.noreply.github.com>
2025-05-17 09:53:35 +0700

    [SDNPRJ-8][UI][fix] Update HomePage.jsx

--- frontend/src/components/pages/home/pages/HomePage.jsx
+++ frontend/src/components/pages/home/pages/HomePage.jsx
@@ -1,12 +1,5 @@
-// const HomePage = () => {
-//   return <div>HomePage</div>;
-// };
-
-// export default HomePage;
-
-import AppHeader from "../../../common/components/Header";
 const HomePage = () => {
-  return <div><AppHeader /></div>;
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
