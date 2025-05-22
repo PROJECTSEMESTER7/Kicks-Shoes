cf8a77dc84092c7126d25ed58ac15704568dc9f3
Tran Phuc Tien <phuctienrt@gmail.com>
2025-05-22 15:44:50 +0700

    [KICKS-15][UI][feat] Add product card components

---
+++ frontend/src/components/common/components/ProductCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./ProductCard.css";
+
+const ProductCard = ({ product }) => {
+  const { image, isNew, name, price, onViewProduct } = product;
+  return (
+    <div className="product-card">
+      <div className="product-card__image-container">
+        {isNew && <div className="product-card__badge">New</div>}
+        <img src={image} alt={name} className="product-card__image" />
+      </div>
+      <div className="product-card__info">
+        <h2 className="product-card__name">{name}</h2>
+      </div>
+      <div className="product-card__footer">
+        <button className="product-card__button" onClick={onViewProduct}>
+          <div className="product-card__button-inner">
+            <span>VIEW PRODUCT -</span>
+            <span className="product-card__price">${price}</span>
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
