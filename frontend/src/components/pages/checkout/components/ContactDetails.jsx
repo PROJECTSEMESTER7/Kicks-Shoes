a49034d1379e8d0874167085343914aec52148a9
CuongDangCode <cuongpro3008@gmail.com>
2025-05-22 23:03:53 +0700

    [KICKS-5][UI][fix]Clean code

--- frontend/src/components/pages/checkout/components/ContactDetails.jsx
+++ frontend/src/components/pages/checkout/components/ContactDetails.jsx
@@ -1,25 +1,3 @@
-// import { Card, Typography, Form, Input } from "antd"
-
-// const { Title, Text, Paragraph } = Typography
-
-// export default function ContactDetails({ form }) {
-//   return (
-//     <Card style={{ background: "#e7e7e3", marginBottom: "24px", border: "none" }}>
-//       <Title level={4} style={{ marginTop: 0, marginBottom: "16px" }}>
-//         Contact Details
-//       </Title>
-//       <Paragraph style={{ marginBottom: "16px" }}>
-//         We will use these details to keep you inform about your delivery.
-//       </Paragraph>
-
-//       <Form form={form} layout="vertical" >
-//         <Form.Item name="email" style={{ marginBottom: "24px", borderColor: "black" }}>
-//           <Input placeholder="Email" size="large" style={{ borderColor: "black" }} />
-//         </Form.Item>
-//       </Form>
-//     </Card>
-//   )
-// }
 import { Card, Typography, Form, Input } from "antd"
 import "./ContactDetails.css"
 
