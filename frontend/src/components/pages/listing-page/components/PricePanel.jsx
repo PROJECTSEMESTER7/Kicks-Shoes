0087bd5f85ef4a45bcf54de365a785d0d798fc28
An Nguyen <anndb1903@gmail.com>
2025-05-26 17:31:08 +0700

    [KICKS-16][UI][fix] FIX do not hide price range

--- frontend/src/components/pages/listing-page/components/PricePanel.jsx
+++ frontend/src/components/pages/listing-page/components/PricePanel.jsx
@@ -1,12 +1,12 @@
 import { Slider } from "antd";
 
-const PricePanel = ({ priceRange }) => (
+const PricePanel = ({ priceRange, isOpen }) => (
   <Slider
     range
     defaultValue={priceRange}
     min={0}
     max={1000}
-    tooltip={{ open: true }}
+    tooltip={{ open: isOpen }}
   />
 );
 
